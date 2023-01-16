f45d23b032c5fde1f18378c787701de3a2b53c7c
lee-song-yi <okpo5071@gmail.com>
2023-01-17 08:48:50 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+<p>안녕하세요</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+//import BaseTag from "./components/BaseTag.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style module>
+.container p{color:var(--base-color-blue-500);}
 </style>
